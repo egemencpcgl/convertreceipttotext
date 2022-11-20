860ab0ab88b8ebd61cf59292ef1d24d39040c0a9
Egemen Çapacıoğlu <42514430+egemencpcgl@users.noreply.github.com>
2022-11-20 21:06:54 +0300

    Add files via upload

---
+++ ConvertReceiptToTextSln/ConvertReceiptToText/Model/Receipt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertReceiptToText.Model
+{
+    public class Receipt
+    {
+        public string? locale { get; set; }
+        public string description { get; set; }
+        public BoundingPoly? boundingPoly { get; set; }
+    }
+    public class BoundingPoly
+    {
+        public List<Vertices> vertices { get; set; }
+    }
+    public class Vertices
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
