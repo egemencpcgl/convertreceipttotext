92f8bdb1275ff64679ae46436b1937f4eb832c39
Egemen Çapacıoğlu <42514430+egemencpcgl@users.noreply.github.com>
2022-11-20 21:29:47 +0300

    Add files via upload

---
+++ ConvertReceiptToTextSln/ConvertReceiptToText/ReceiptOperation/ReceiptManager.cs
@@ -0,0 +1,64 @@
+﻿using ConvertReceiptToText.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConvertReceiptToText.ReceiptOperation
+{
+    public class ReceiptManager
+    {
+        #region Private Prop
+        private List<Receipt> receiptInformation { get; set; }
+        #endregion
+
+        #region Public Prop
+        public string path = Directory.GetCurrentDirectory() + "/response.json";
+
+        public List<Receipt> ReceiptInformation 
+        {
+            get { return receiptInformation; }
+            set { receiptInformation = value; }
+        }
+        #endregion
+
+        #region Singleton
+        private static ReceiptManager receiptManager;
+        public static ReceiptManager Receiptmanager
+        {
+            get
+            {
+                if (receiptManager == null)
+                {
+                    receiptManager = new ReceiptManager();
+                }
+                return receiptManager;
+            }
+        }
+        #endregion
+
+        public void ReadReceiptFile()
+        {
+            try
+            {
+                if (System.IO.File.Exists(path))
+                {
+                    using (StreamReader reader = new StreamReader(path))
+                    {
+                        string json = reader.ReadToEnd();
+                        ReceiptInformation = JsonConvert.DeserializeObject<List<Receipt>>(json.ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File doesn't exists");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something happened wrong during of read or convert file: "+e.Message);
+            }
+        }
+
+    }
+}
---
+++ ConvertReceiptToTextSln/ConvertReceiptToText/SelectPath.cs
@@ -0,0 +1,34 @@
+﻿using ConvertReceiptToText.ReceiptOperation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertReceiptToText
+{
+    public partial class SelectPath : Form
+    {
+        OpenFileDialog file;
+
+        public SelectPath()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                ReceiptManager.Receiptmanager.path = file.FileName;
+                Form1 form = new Form1();
+                form.Show();
+            }
+        }
+    }
+}
